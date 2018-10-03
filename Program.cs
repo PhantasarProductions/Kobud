8b9d6efdab3ab5aad5527bd7aab21e49c2ed911f
Tricky1975 <facebook@tbbs.nl>
2018-10-04 00:29:25 +0200

    Load JCR

--- Program.cs
+++ Program.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using Gtk;
 using UseJCR6;
+using TrickyUnits;
+using TrickyUnits.GTK;
 
 namespace Kobud
 {
     class MainClass
     {
+        static TJCRDIR Assets;
+
         static object Init_JCR6() => new JCR6_lzma();
+
+        static void Load_JCR6() { 
+            Assets = JCR6.Dir(Dirry.C("$AppDir$/jcr/Kobud.jcr"));
+            if (Assets == null) {
+                QuickGTK.Error(JCR6.JERROR);
+                throw new Exception(JCR6.JERROR);
+            }
+        }
 
         static void Init_GUI()
         {
             Application.Init();
             MainWindow win = new MainWindow();
+            QuickGTK.MyMainWindow = win;
+            win.Title = "Kobud";
             win.ShowAll();
         }
 
@@ -21,6 +35,7 @@ namespace Kobud
         static public void Main(){
             Init_JCR6();
             Init_GUI();
+            Load_JCR6();
             Run();
         }
     }
